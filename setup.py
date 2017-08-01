4c64bbbff016b9252817e7b67d79d0861055c8cf
Ted Chen <tedchen@KKStreamde-MacBook-Pro.local>
2017-08-01 15:31:58 +0800

    Change project structure

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='restapi',
-    version='0.2',
-    install_requires=['requests']
+    version='0.3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    provides=['hamcrest'],
+    install_requires=['setuptools', 'requests']
 )
